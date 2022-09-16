1fe1feb9a48b74600e6ef3caf0d0ef7e043876d5
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2022-09-16 23:43:26 +0900

    Fix test for (*Reader).Read

--- cmd/read_test.go
+++ cmd/read_test.go
@@ -1,117 +1,133 @@
 package main
 
 import (
+	"bytes"
 	"io"
-	"net"
 	"strings"
 	"testing"
-	"time"
 )
 
 func TestReaderRead(t *testing.T) {
-	tests := []struct {
-		inPSize   int
-		inRdBuf   string
-		inRecvBuf string
-		outP      string
-		outN      int
-		outErr    error
-		outRdBuf  string
+	cases := []struct {
+		inPSize int
+		inRxTxt string
+		inBuf   []byte
+		wantP   []byte
+		wantN   int
+		wantErr error
+		wantBuf []byte
 	}{
 		{
-			inPSize:   0,
-			inRdBuf:   "",
-			inRecvBuf: "",
-			outP:      "",
-			outN:      0,
-			outErr:    nil,
-			outRdBuf:  "",
+			inPSize: 0,
+			inRxTxt: "",
+			inBuf:   []byte{0x42, 0x43},
+			wantP:   []byte{},
+			wantN:   0,
+			wantErr: nil,
+			wantBuf: []byte{0x42, 0x43},
 		},
 		{
-			inPSize:   1,
-			inRdBuf:   "TEST",
-			inRecvBuf: "",
-			outP:      "T",
-			outN:      1,
-			outErr:    nil,
-			outRdBuf:  "EST",
+			inPSize: 1,
+			inRxTxt: "",
+			inBuf:   []byte{0x42, 0x43},
+			wantP:   []byte{0x42},
+			wantN:   1,
+			wantErr: nil,
+			wantBuf: []byte{0x43},
 		},
 		{
-			inPSize:   5,
-			inRdBuf:   "TEST",
-			inRecvBuf: "",
-			outP:      "TEST\x00",
-			outN:      4,
-			outErr:    nil,
-			outRdBuf:  "",
+			inPSize: 2,
+			inRxTxt: "",
+			inBuf:   []byte{0x42, 0x43},
+			wantP:   []byte{0x42, 0x43},
+			wantN:   2,
+			wantErr: nil,
+			wantBuf: []byte{},
 		},
 		{
-			inPSize:   1,
-			inRdBuf:   "",
-			inRecvBuf: "",
-			outP:      "\x00",
-			outN:      0,
-			outErr:    io.ErrUnexpectedEOF,
-			outRdBuf:  "",
+			inPSize: 3,
+			inRxTxt: "",
+			inBuf:   []byte{0x42, 0x43},
+			wantP:   []byte{0x42, 0x43, 0x00},
+			wantN:   2,
+			wantErr: nil,
+			wantBuf: []byte{},
 		},
 		{
-			inPSize:   1,
-			inRdBuf:   "",
-			inRecvBuf: string(chunkPrefix) + string(chunkSuffix),
-			outP:      "\x00",
-			outN:      0,
-			outErr:    nil,
-			outRdBuf:  "",
+			inPSize: 1,
+			inRxTxt: "",
+			inBuf:   []byte{},
+			wantP:   []byte{0x00},
+			wantN:   0,
+			wantErr: io.ErrUnexpectedEOF,
+			wantBuf: []byte{},
 		},
 		{
-			inPSize:   1,
-			inRdBuf:   "",
-			inRecvBuf: string(chunkPrefix) + "TEST" + string(chunkSuffix),
-			outP:      "T",
-			outN:      1,
-			outErr:    nil,
-			outRdBuf:  "EST",
+			inPSize: 1,
+			inRxTxt: "GET  HTTP/1.1\r\n",
+			inBuf:   []byte{},
+			wantP:   []byte{0x00},
+			wantN:   0,
+			wantErr: nil,
+			wantBuf: []byte{},
 		},
 		{
-			inPSize:   5,
-			inRdBuf:   "",
-			inRecvBuf: string(chunkPrefix) + "TEST" + string(chunkSuffix),
-			outP:      "TEST\x00",
-			outN:      4,
-			outErr:    nil,
-			outRdBuf:  "",
+			inPSize: 1,
+			inRxTxt: "GET BC HTTP/1.1\r\n",
+			inBuf:   []byte{},
+			wantP:   []byte{0x42},
+			wantN:   1,
+			wantErr: nil,
+			wantBuf: []byte{0x43},
+		},
+		{
+			inPSize: 2,
+			inRxTxt: "GET BC HTTP/1.1\r\n",
+			inBuf:   []byte{},
+			wantP:   []byte{0x42, 0x43},
+			wantN:   2,
+			wantErr: nil,
+			wantBuf: []byte{},
+		},
+		{
+			inPSize: 3,
+			inRxTxt: "GET BC HTTP/1.1\r\n",
+			inBuf:   []byte{},
+			wantP:   []byte{0x42, 0x43, 0x00},
+			wantN:   2,
+			wantErr: nil,
+			wantBuf: []byte{},
 		},
 	}
-	for i, tt := range tests {
-		conn := &mockConnOld{
-			rd:       strings.NewReader(tt.inRecvBuf),
-			closed:   false,
-			deadline: time.Time{},
+
+	for i, c := range cases {
+		inRx := NewReceiver(&mockListener{
+			acceptConn: &mockConn{
+				readInner: strings.NewReader(c.inRxTxt),
+				closeErr:  nil,
+			},
+			acceptErr: nil,
+			closeErr:  nil,
+		})
+		inRd := &Reader{
+			recv: inRx,
+			buf:  c.inBuf,
 		}
-		lis := &mockListenerOld{
-			conns: []net.Conn{conn},
+
+		gotP := make([]byte, c.inPSize)
+		gotN, gotErr := inRd.Read(gotP)
+
+		if !bytes.Equal(gotP, c.wantP) {
+			t.Errorf("case %d: p: expected %#v, got %#v", i, c.wantP, gotP)
 		}
-		recv := &Receiver{
-			lis: lis,
-			cg:  CloseGroup{},
+		if gotN != c.wantN {
+			t.Errorf("case %d: n: expected %d, got %d", i, c.wantN, gotN)
 		}
-		rd := &Reader{
-			recv: recv,
-			buf:  []byte(tt.inRdBuf),
+		if gotErr != c.wantErr {
+			t.Errorf(`case %d: err: expected "%s", got "%s"`, i, c.wantErr, gotErr)
 		}
-		p := make([]byte, tt.inPSize)
-		n, err := rd.Read(p)
-		if string(p) != tt.outP {
-			t.Errorf("case %d: expected \"%s\", got \"%s\"", i, string(p), tt.outP)
-		}
-		if n != tt.outN {
-			t.Errorf("case %d: expected %d, got %d", i, n, tt.outN)
-		}
-		if err != tt.outErr {
-			t.Errorf("case %d: expected %#v, got %#v", i, err, tt.outErr)
-		}
-		if string(rd.buf) != tt.outRdBuf {
-			t.Errorf("case %d: expected \"%s\", got \"%s\"", i, string(rd.buf), tt.outRdBuf)
+		if !bytes.Equal(inRd.buf, c.wantBuf) {
+			t.Errorf("case %d: buf: expected %#v, got %#v", i, c.wantBuf, inRd.buf)
 		}
 	}
 }
