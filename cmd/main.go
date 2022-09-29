3a62a91f7b99930d2b21c11b7abbeb0a8e794045
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2022-09-30 00:03:41 +0900

    Remove cmd functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,4 @@
 package main
 
-import (
-	"flag"
-	"log"
-	"os"
-)
-
 func main() {
-	port := flag.Int("port", DefaultPort, "listen port")
-	root := flag.String("root", DefaultRoot, "where to write files")
-	title := flag.String("title", DefaultTitle, "default title of data")
-	ext := flag.String("ext", DefaultExt, "file extension")
-	flag.Parse()
-
-	cfg := NewServerConfig()
-	cfg.Root = *root
-	cfg.Title = *title
-	cfg.Ext = *ext
-	srv := NewServer(cfg)
-	err := srv.ListenAndServe(*port)
-	log.Println(err)
-	os.Exit(1)
 }
-
-const (
-	DefaultPort     = 58592
-	DefaultRoot     = "."
-	DefaultTitle    = "telemetory"
-	DefaultExt      = ".csv"
-	DefaultDirMode  = os.FileMode(0777)
-	DefaultFileMode = os.FileMode(0666)
-)
--- cmd/serve.go
+++
@@ -1,5 +0,0 @@
-package main
-
-type Service interface {
-	Serve(req []byte) ([]byte, error)
-}
